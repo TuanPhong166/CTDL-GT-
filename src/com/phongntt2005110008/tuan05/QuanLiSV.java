c6ef27885f58e56bdd7450b02f666982e61c38d1
Tuanphong166 <mobiistar2st@gmail.com>
2021-10-17 20:52:02 +0700

    cập nhật  mới

--- src/com/phongntt2005110008/tuan05/QuanLiSV.java
+++ src/com/phongntt2005110008/tuan05/QuanLiSV.java
@@ -3,30 +3,24 @@ package com.phongntt2005110008.tuan05;
 import java.util.Scanner;
 
 public class QuanLiSV {
-    static Scanner input = new Scanner(System.in);
     public static final String remove = null;
-    // bài làm chưa xong 
-    public static String hoTen ;
-    public static Double diem ;
-
-    public QuanLiSV(){}
-
-    public QuanLiSV(String t , double d ){
-        hoTen = t ;
-        diem = d ;
-    }
+    String hoVaTen;
+    Double diemTB;
+    Scanner input = new Scanner(System.in);
     
+    QuanLiSV(){
 
+    }
     void inThongTin(){
-        System.out.println("Họ và tên sinh viên : "+hoTen);
-        System.out.println("Điểm số  : "+diem);
+        System.out.println("Họ và tên: " +hoVaTen);
+        System.out.println("Điểm trung bình: " +diemTB);
+        System.out.println("");
     }
     void nhapThongTin(){
-        System.out.print("Nhập họ và tên: ");
-        hoTen = input.nextLine();
-        System.out.print("Nhập điểm trung bình: ");
-        diem = input.nextDouble();
-
+            System.out.print("Nhập họ và tên: ");
+            hoVaTen = input.nextLine();
+            System.out.print("Nhập điểm trung bình: ");
+            diemTB = input.nextDouble();
+            System.out.println("");
     }
-       
     }
