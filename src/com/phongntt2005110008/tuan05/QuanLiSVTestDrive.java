be773618ffc9771e00007c1ea5489ea75431bee7
Tuanphong166 <mobiistar2st@gmail.com>
2021-10-17 20:51:23 +0700

    cập nhật mới

--- src/com/phongntt2005110008/tuan05/QuanLiSVTestDrive.java
+++ src/com/phongntt2005110008/tuan05/QuanLiSVTestDrive.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 
 public class QuanLiSVTestDrive {
 
-    static ArrayList<QuanLiSV> listSV = new ArrayList<>();
     static Scanner input = new Scanner(System.in);
-
+    
+    static ArrayList<QuanLiSV> DSSV = new ArrayList<>();
     public static void main(String[] args) {
-        int chon;
+        int chose;
         do{
             System.out.println("==========MENU==========");
             System.out.println(">>1. Nhập thông tin sinh viên.");
@@ -20,106 +20,106 @@ public class QuanLiSVTestDrive {
             System.out.println(">>6. Tìm và xóa thông tin.");
             System.out.println(">>7. Kết thúc.");
             System.out.print("Nhập vào sự lựa chọn: ");
-            chon = input.nextInt();
-            switch(chon){
+            chose = input.nextInt();
+            switch(chose){
                 case 1: 
-                        nhap();
+                        input();
                         break;
                 case 2: 
-                        xuatDS();
+                        output();
                         break;
                 case 3:
-                        xuatTheoDiem();
+                        outputByPoint();
                         break;
                 case 4: 
-                        tim();
+                        search();
                         break;
                 case 5: 
-                        timVaSua();
+                        findAndFix();
                         break;
                 case 6: 
-                        timVaXoa();
+                        findAndDelete();
                         break;
                 case 7:
                         System.out.println("KẾT THÚC!");
                         break;
                 default: 
-                        System.out.println("- Lựa chọn không hợp lệ!");
+                        System.out.println("Lựa chọn không hợp lệ!");
 
             }
-            System.out.print("- Quay lại MENU(1:yes/0:no): ");
-            chon = input.nextInt();
-        }while(chon == 1);
+            System.out.print("- Quay lại MENU(1:Yes / 0:No): ");
+            chose = input.nextInt();
+        }while(chose == 1);
 
     }
-    static void nhap(){
-        int chon;
+    static void input(){
+        int yes;
         do{
                 System.out.println("- Nhập thông tin của sinh viên: ");
+                // tạo đối tượng sinh viên
                 QuanLiSV sv = new QuanLiSV();
                 sv.nhapThongTin();
-                listSV.add(sv);
-                System.out.print("- Nhập thêm(1:yes/0:no): ");
-                chon = input.nextInt();
+                System.out.println();
+                DSSV.add(sv);
+                System.out.print("- Nhập thêm(1:Yes / 0:No): ");
+                yes = input.nextInt();
                 input.nextLine();
-        }while(chon == 1);
+        }while(yes == 1);
     }
-    static void xuatDS(){
-        for(QuanLiSV x : listSV){
+    static void output(){
+        for(QuanLiSV x : DSSV){
             x.inThongTin();
         }
     }
-    static void xuatTheoDiem(){
-        System.out.print("- Nhập điểm min: ");
+    static void outputByPoint(){ // xuát theo điểm
+        System.out.print("Nhập điểm min: ");
         double min = input.nextDouble();
-        System.out.print("- Nhập điểm max: ");
+        System.out.print("Nhập điểm max: ");
         double max = input.nextDouble();
         System.out.println("-Danh sách sinh viên trong khoảng " +min+ "đến" +max+ "là: ");
-        for(QuanLiSV x : listSV){
-                if(x.diem >= min && x.diem <= max){
+        for(QuanLiSV x : DSSV){
+                if(x.diemTB >= min && x.diemTB <= max){
                         x.inThongTin();
                 }
         }
     }
-    static void tim(){
-        System.out.print("- Nhập họ tên sinh viên cần tìm: ");
+    static void search(){ // tìm kiếm 
+        System.out.print("Nhập họ tên sinh viên cần tìm: ");
         input.nextLine();
         String hoTen = input.nextLine();
-        for(QuanLiSV x : listSV){
-                if(x.hoTen.contains(hoTen)){
+        for(QuanLiSV x : DSSV){
+                if(x.hoVaTen.contains(hoTen)){ //.contains kiểm tra sự tồn tại
                         System.out.println("-----Thông tin sinh viên cần tìm-----");
                         x.inThongTin();
                 }
         }  
     }
-    static void timVaSua(){
-        System.out.print("- Nhập tên sinh viên cần sửa điểm: ");
+    static void findAndFix(){ // tìm và sửa
+        System.out.print("Nhập tên sinh viên cần sửa điểm: ");
         input.nextLine();
         String hvt = input.nextLine();
-        for(QuanLiSV x : listSV){
-                if(x.hoTen.contains(hvt)){
-                        System.out.print("  + Nhập điểm mới: ");
-                        x.diem = input.nextDouble();
+        for(QuanLiSV x : DSSV){
+                if(x.hoVaTen.contains(hvt)){  //.contains kiểm tra sự tồn tại
+                        System.out.print("Nhập điểm mới: ");
+                        x.diemTB = input.nextDouble();
                         System.out.println("-----Sau khi sửa-----");
                         x.inThongTin();
                 }
         }
     }
-    static void timVaXoa(){
-            System.out.print("- Nhập tên sinh viên cần xóa: ");
+    static void findAndDelete(){ // tìm và xóa.
+            System.out.print("Nhập tên sinh viên cần xóa: ");
             input.nextLine();
             String ht = input.nextLine();
-            for(QuanLiSV x : listSV){
-                    if(x.hoTen.equals(ht)){
-                        listSV.remove(x);
-                        System.out.println("- Xóa thành công!");
+            for(QuanLiSV x : DSSV){
+                    if(x.hoVaTen.equals(ht)){
+                        DSSV.remove(x); // .remove phần tử
+                        System.out.println("Xóa thành công!");
                         break;
                     }
             }
-    }
-    
 
-    
+    }
 }
     
 
