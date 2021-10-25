27e3adf9abf71362716a41d73fc2d806909fc712
Tuanphong166 <mobiistar2st@gmail.com>
2021-10-25 12:22:49 +0700

    bai 2

--- src/com/phongntt2005110008/kiemtragiuaky/Node.java
+++ src/com/phongntt2005110008/kiemtragiuaky/Node.java
@@ -2,19 +2,27 @@ package com.phongntt2005110008.kiemtragiuaky;
 
 public class Node {
      // Nội dung
-     int data ;
+     int maTao ;
+     double khoiLuong ;
+     String color ;
      // Liên kết đén nút tiếp theo
      Node next ; // Biến tham chiếu kiểu Node
  
      public Node(){} // hàm tạo rỗng
      
      public Node(int d){  // hàm tạo 1 tham chiếu 
-           data = d ;
+           maTao = d ;
      }
  
-     public Node(int d , Node n){ // hàm tạo 2 tham chiếu 
-         data = d ;
-         next =  n ;
+     public Node(int d , double kL){ // hàm tạo 2 tham chiếu 
+         maTao = d ;
+         khoiLuong = kL ;
+     }
+     public Node(int d , double kL , String c , Node n){
+         maTao = d ;
+         khoiLuong = kL ;
+         color = c ;
+         next = n;
      }
     
 }
